35099ea45c59aa963b4fdee22e96e5d22b9039f6
Santiago Fernandez <santiago.fernandez180603@gmail.com>
2024-08-08 12:04:03 -0400

    Added avatar and name feature

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Box, VStack, Heading, Text } from '@chakra-ui/react';
+import { Box, VStack, HStack, Text, Avatar } from '@chakra-ui/react';
 
 const Profile = () => {
   return (
-    <Box p={5}>
+    <Box p={5} maxWidth="100%" width="400px">
       <VStack spacing={4} align="stretch">
-        <Heading size="lg">Profile</Heading>
-        <Text>Welcome to your profile page. This is where you can view and edit your personal information.</Text>
-        {/* Add more profile-related content here */}
+        <HStack spacing={4} align="center">
+          <Avatar
+            size='2xl'
+            name='Prosper Otemuyiwa'
+            src='https://bit.ly/prosper-baba'
+            flexShrink={0}
+          />
+          <Box flex={1} minWidth={0}>
+            <Text fontSize='2xl' isTruncated>
+              Quandale Dingle
+            </Text>
+          </Box>
+        </HStack>
       </VStack>
     </Box>
   );
